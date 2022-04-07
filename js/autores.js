44dcf167e5d2964c83d60949a651d9009ba65f05
amanda300996 <amandabm3009@gmail.com>
2022-04-06 21:37:01 -0600

    con los botones que pablo pidio

--- js/autores.js
+++ js/autores.js
@@ -1,25 +1,89 @@
 let listaAutores = [{
-    'nombre': 'Gabriel ',
-    'apellido': 'García Marquez',
-    'nacionalidad': 1,
-    'FechaNacimiento': '6 de marzo 1927',
-    'FechaDefunción': 'García Marquez',
-    'Biografia': 'aqui va la biografía',
-}, {
-    'nombre': 'JK ',
-    'apellido': 'Rowling',
-    'nacionalidad': 2,
-    'FechaNacimiento': '6 de marzo 1960',
-    'FechaDefunción': '',
-    'Biografia': 'aqui va la biografía',
-}, {
-    'nombre': 'Edgar ',
-    'apellido': 'Allan Poe',
-    'nacionalidad': 2,
-    'FechaNacimiento': '6 de febrero 1950',
-    'FechaDefunción': '8 de marzo de 2011',
-    'Biografia': 'aqui va la biografía',
-}]; // json se sacan con alt 91, cada valor tiene una posición e iniciaen 0;
+        'nombre': 'Gabriel ',
+        'apellido': 'García Marquez',
+        'nacionalidad': "Costa Rica",
+        'FechaNacimiento': '6 de marzo 1927',
+        'FechaDefunción': '12 de abril de 2011',
+
+    }, {
+        'nombre': 'JK ',
+        'apellido': 'Rowling',
+        'nacionalidad': "Costa Rica",
+        'FechaNacimiento': '6 de marzo 1960',
+        'FechaDefunción': 'N.A',
+
+    },
+    {
+        'nombre': 'Edgar ',
+        'apellido': 'Allan Poe',
+        'nacionalidad': "Costa Rica",
+        'FechaNacimiento': '6 de febrero 1950',
+        'FechaDefunción': '8 de marzo de 2011',
+    },
+    {
+        'nombre': 'Edgar ',
+        'apellido': 'Allan Poe',
+        'nacionalidad': "Costa Rica",
+        'FechaNacimiento': '6 de febrero 1950',
+        'FechaDefunción': '8 de marzo de 2011',
+    },
+    {
+        'nombre': 'Edgar ',
+        'apellido': 'Allan Poe',
+        'nacionalidad': "Costa Rica",
+        'FechaNacimiento': '6 de febrero 1950',
+        'FechaDefunción': '8 de marzo de 2011',
+    },
+    {
+        'nombre': 'Edgar ',
+        'apellido': 'Allan Poe',
+        'nacionalidad': "Costa Rica",
+        'FechaNacimiento': '6 de febrero 1950',
+        'FechaDefunción': '8 de marzo de 2011',
+    },
+    {
+        'nombre': 'Edgar ',
+        'apellido': 'Allan Poe',
+        'nacionalidad': "Costa Rica",
+        'FechaNacimiento': '6 de febrero 1950',
+        'FechaDefunción': '8 de marzo de 2011',
+    },
+
+    {
+        'nombre': 'JK ',
+        'apellido': 'Rowling',
+        'nacionalidad': "Costa Rica",
+        'FechaNacimiento': '6 de marzo 1960',
+        'FechaDefunción': '8 de marzo de 2011',
+    },
+    {
+        'nombre': 'JK ',
+        'apellido': 'Rowling',
+        'nacionalidad': "Costa Rica",
+        'FechaNacimiento': '6 de marzo 1960',
+        'FechaDefunción': '8 de marzo de 2011',
+
+    }, {
+        'nombre': 'Edgar ',
+        'apellido': 'Allan Poe',
+        'nacionalidad': "Costa Rica",
+        'FechaNacimiento': '6 de febrero 1950',
+        'FechaDefunción': '8 de marzo de 2011',
+    },
+
+    {
+        'nombre': 'Edgar ',
+        'apellido': 'Allan Poe',
+        'nacionalidad': "Costa Rica",
+        'FechaNacimiento': '6 de febrero 1950',
+        'FechaDefunción': '8 de marzo de 2011',
+    }
+
+
+
+
+
+]; // json se sacan con alt 91, cada valor tiene una posición e iniciaen 0;
 
 listaAutores.forEach(autor => {
     console.log(autor.nombre);
--- js/lista_autores.js
+++ js/lista_autores.js
@@ -1,92 +1,97 @@
-const cuerpoTabla = document.querySelector("#tbl-libros tbody"); // se usa selectores de css
+const cuerpoTabla = document.querySelector("#tbl-libros tbody");
+// se usa selectores de css
 
 document.getElementById("btn-agregar-autor").addEventListener('click', () => {
     window.location.href = "registro_autor.html";
 
 });
 
-document.getElementById("btn-editar-autor").addEventListener('click', () => {
-    window.location.href = "registro_autor.html";
-
-});
-
-document.getElementById("btn-eliminar-autor").addEventListener('click', () => {
-    Swal.fire({
-        'icon': 'warning',
-        'title': '¿Seguro que desea eliminar registro?',
-        'text': 'Esta acción no se puede deshacer'
-    });
-});
-
-
-
-
 const mostrarDatos = () => {
     cuerpoTabla.innerHTML = '';
-    let fila = cuerpoTabla.insertRow(); // CREO LA FILA
+    listaAutores.forEach(elemento => {
+        // creo los botones
+        let botonBiografia = document.createElement("button");
+        botonBiografia.textContent = "Perfil"
+        let spanBiografia = document.createElement("span");
+        let iconoBiografia = document.createElement("i");
 
-    fila.insertCell().innerText = "Gabriel";
-    fila.insertCell().innerText = "García Marquez"; // NOMBRO LA FILA Y SUMA COLUMNAS
-    fila.insertCell().innerText = "Colombia";
-    fila.insertCell().innerText = "6 de marzo 1927";
-    fila.insertCell().innerText = "17 de abril 2014";
+        let botonEditar = document.createElement("button");
+        let spanEditar = document.createElement("span");
+        let iconoEditar = document.createElement("i");
 
-    let fila2 = cuerpoTabla.insertRow(); // CREO LA FILA
-    fila2.insertCell().innerText = "Gabriel";
-    fila2.insertCell().innerText = "García Marquez";
-    fila2.insertCell().innerText = "Colombia";
-    fila2.insertCell().innerText = "6 de marzo 1927";
-    fila2.insertCell().innerText = "17 de abril 2014";
-    let fila3 = cuerpoTabla.insertRow(); // CREO LA FILA
-    fila3.insertCell().innerText = "Gabriel";
-    fila3.insertCell().innerText = "García Marquez";
-    fila3.insertCell().innerText = "Colombia";
-    fila3.insertCell().innerText = "6 de marzo 1927";
-    fila3.insertCell().innerText = "17 de abril 2014";
-    let fila4 = cuerpoTabla.insertRow(); // CREO LA FILA
-    fila4.insertCell().innerText = "Gabriel";
-    fila4.insertCell().innerText = "García Marquez";
-    fila4.insertCell().innerText = "Colombia";
-    fila4.insertCell().innerText = "6 de marzo 1927";
-    fila4.insertCell().innerText = "17 de abril 2014";
-    let fila5 = cuerpoTabla.insertRow(); // CREO LA FILA
-    fila5.insertCell().innerText = "Gabriel";
-    fila5.insertCell().innerText = "García Marquez";
-    fila5.insertCell().innerText = "Colombia";
-    fila5.insertCell().innerText = "6 de marzo 1927";
-    fila5.insertCell().innerText = "17 de abril 2014";
-    let fila6 = cuerpoTabla.insertRow(); // CREO LA FILA
-    fila6.insertCell().innerText = "Gabriel";
-    fila6.insertCell().innerText = "García Marquez";
-    fila6.insertCell().innerText = "Colombia";
-    fila6.insertCell().innerText = "6 de marzo 1927";
-    fila6.insertCell().innerText = "17 de abril 2014";
-    let fila7 = cuerpoTabla.insertRow(); // CREO LA FILA
-    fila7.insertCell().innerText = "Gabriel";
-    fila7.insertCell().innerText = "García Marquez";
-    fila7.insertCell().innerText = "Colombia";
-    fila7.insertCell().innerText = "6 de marzo 1927";
-    fila7.insertCell().innerText = "17 de abril 2014";
-    let fila8 = cuerpoTabla.insertRow(); // CREO LA FILA
-    fila8.insertCell().innerText = "Gabriel";
-    fila8.insertCell().innerText = "García Marquez";
-    fila8.insertCell().innerText = "Colombia";
-    fila8.insertCell().innerText = "6 de marzo 1927";
-    fila8.insertCell().innerText = "17 de abril 2014";
-    let fila9 = cuerpoTabla.insertRow(); // CREO LA FILA
-    fila9.insertCell().innerText = "Gabriel";
-    fila9.insertCell().innerText = "García Marquez";
-    fila9.insertCell().innerText = "Colombia";
-    fila9.insertCell().innerText = "6 de marzo 1927";
-    fila9.insertCell().innerText = "17 de abril 2014";
-    let fila10 = cuerpoTabla.insertRow(); // CREO LA FILA
-    fila10.insertCell().innerText = "Gabriel";
-    fila10.insertCell().innerText = "García Marquez";
-    fila10.insertCell().innerText = "Colombia";
-    fila10.insertCell().innerText = "6 de marzo 1927";
-    fila10.insertCell().innerText = "17 de abril 2014";
+        let botonEliminar = document.createElement("button");
+        let spanEliminar = document.createElement("span");
+        let iconoEliminar = document.createElement("i");
 
+        // le doy estilos
+
+        iconoEliminar.classList.add('fa-solid');
+        iconoEliminar.classList.add('fa-trash-can');
+        iconoEliminar.classList.add('fa-1l');
+        botonEliminar.classList.add('icono-eliminar');
+        botonEliminar.name = "boton-eliminar";
+
+        iconoEditar.classList.add('fa-solid');
+        iconoEditar.classList.add('fa-pen-to-square');
+        iconoEditar.classList.add('fa-1l');
+        iconoEditar.classList.add('icono-editar');
+        botonEditar.name = "boton-editar";
+
+        botonBiografia.name = "boton-biografia";
+
+        //establece jerarquia
+        spanEliminar.appendChild(iconoEliminar);
+        botonEliminar.appendChild(spanEliminar);
+
+        spanEditar.appendChild(iconoEditar);
+        botonEditar.appendChild(spanEditar);
+
+        let fila = cuerpoTabla.insertRow(); // CREO LA FILA
+        fila.insertCell().innerText = elemento.nombre;
+        fila.insertCell().innerText = elemento.apellido;
+        // NOMBRO LA FILA Y SUMA COLUMNAS
+        fila.insertCell().innerText = elemento.nacionalidad;
+        fila.insertCell().innerText = elemento.FechaNacimiento;
+        fila.insertCell().innerText = elemento.FechaDefunción;
+
+        fila.insertCell().appendChild(botonBiografia);
+        fila.insertCell().appendChild(botonEditar);
+        fila.insertCell().appendChild(botonEliminar);
+
+    });
 
 };
+
+
+const botonesEliminar = document.getElementsByName('boton-eliminar');
+const botonesEditar = document.getElementsByName('boton-editar');
+const botonesBiografia = document.getElementsByName('boton-biografia');
+
+const accion = () => {
+    Swal.fire({
+        'icon': 'error',
+        'title': 'Método eliminado!',
+        'text': 'Se ha eliminado el método de pago.'
+    });
+};
+
+const formulario = () => {
+    window.location.href = "registro_autor.html";
+};
+const perfilautor = () => {
+    window.location.href = "perfil_autor.html";
+};
+
+botonesEliminar.forEach(boton => {
+    boton.addEventListener('click', accion());
+});
+
+botonesEditar.forEach(boton => {
+    boton.addEventListener('click', formulario());
+});
+
+botonesBiografia.forEach(boton => {
+    boton.addEventListener('click', perfilautor());
+});
 
 mostrarDatos();
