06465f9a81089035bdd8416540eb420ae0358f56
amanda300996 <amandabm3009@gmail.com>
2022-04-24 11:52:34 -0600

    estoy intentando sincronicar mi espacio

---
+++ proyectofinal/js/autores.js
@@ -0,0 +1,26 @@
+let listaAutores = [{
+    'nombre': 'Gabriel ',
+    'apellido': 'García Marquez',
+    'nacionalidad': 1,
+    'FechaNacimiento': '6 de marzo 1927',
+    'FechaDefunción': 'García Marquez',
+    'Biografia': 'aqui va la biografía',
+}, {
+    'nombre': 'JK ',
+    'apellido': 'Rowling',
+    'nacionalidad': 2,
+    'FechaNacimiento': '6 de marzo 1960',
+    'FechaDefunción': '',
+    'Biografia': 'aqui va la biografía',
+}, {
+    'nombre': 'Edgar ',
+    'apellido': 'Allan Poe',
+    'nacionalidad': 2,
+    'FechaNacimiento': '6 de febrero 1950',
+    'FechaDefunción': '8 de marzo de 2011',
+    'Biografia': 'aqui va la biografía',
+}]; // json se sacan con alt 91, cada valor tiene una posición e iniciaen 0;
+
+listaAutores.forEach(autor => {
+    console.log(autor.nombre);
+});
