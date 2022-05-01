58a270f42338e82350defada84d9104edc6201e9
Stf11 <spizarroloaiza@hotmail.com>
2022-05-01 17:30:21 -0600

    formLibros

--- models/libros.model.js
+++ models/libros.model.js
@@ -3,11 +3,15 @@ const mongoose = require('mongoose');
 const schemaLibro = new mongoose.Schema({
     titulo: { type: String, required: true },
     editorial: { type: String, required: true },
-    fotos: { type: String, required: true },
+    portada: { type: String, required: false },
+    contraportada: { type: String, required: false },
     precio: { type: Number, required: true },
     publicacion: { type: Date, required: true },
-    premios: { type: String, required: false },
+    genero: { type: String, required: true },
+    autor: { type: String, required: true },
+    descuento: { type: Number, required: true },
     isbn: { type: Number, required: true, unique: true },
+    premios: { type: Number, required: false },
     sipnosis: { type: String, required: true }
 });
 
