9c65ebf87b6367127e395cd158718f82eedd623f
aljvld03 <101369764+aljvld03@users.noreply.github.com>
2022-05-02 19:05:03 -0600

    nuevoAle

--- api/models/comercios.model.js
+++ api/models/comercios.model.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const schemaComercio = new mongoose.Schema({
     nombrecomercio: { type: String, required: true },
-    cedulajuridica: { type: Number, required: true },
+    cedulajuridica: { type: String, required: true },
     fechainicio: { type: Date, required: true },
     provinciacomercio: { type: String, required: true },
     cantoncomercio: { type: String, required: true },
     distritocomercio: { type: String, required: true },
-    direccioncomercio: { type: Number, required: true },
+    direccioncomercio: { type: String, required: true },
     fotocomercio: { type: String, required: true }
 });
 
