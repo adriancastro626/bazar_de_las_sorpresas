aadaf4cdb227184c45e20a42ca03a4868d7c3e6d
aljvld03 <aljvldcoto03@gmail.com>
2022-05-09 14:53:31 -0600

    ultimo

--- api/models/comercios.model.js
+++ api/models/comercios.model.js
@@ -4,9 +4,9 @@ const schemaComercio = new mongoose.Schema({
     nombrecomercio: { type: String, required: true },
     cedulajuridica: { type: String, required: true },
     fechainicio: { type: Date, required: true },
-    provinciacomercio: { type: String, required: true },
-    cantoncomercio: { type: String, required: true },
-    distritocomercio: { type: String, required: true },
+    provinciacomercio: { type: Number, required: true },
+    cantoncomercio: { type: Number, required: true },
+    distritocomercio: { type: Number, required: true },
     direccioncomercio: { type: String, required: true },
     fotocomercio: { type: String, required: true }
 });
--- public/controllerJS/lista_socios_comerciales.js
+++ public/controllerJS/lista_socios_comerciales.js
@@ -1,22 +1,59 @@
-const btnEditar = document.getElementById('btn-editar-socio');
-const btnEliminar = document.getElementById('btn-eliminar-socio')
-const btnAgregar = document.getElementById('btn-agregar-socio')
+/*const btnEditar = document.getElementById('btn-editar-socio');
+const btnEliminar = document.getElementById('btn-eliminar-socio');*/
+const btnAgregar = document.getElementById('btn-agregar-socio');
+const cuerpoTabla = document.querySelector('#tbl-socios tbody');
+let listaSocios = [];
 
-btnEditar.addEventListener('click', () => {
-    Swal.fire({
-        'icon': 'info',
-        'title': 'Botón de editar seleccionado',
-        'text': 'Redirigiendo a la pagina de edición de socios'
-    });
-});
+const inicializar = async() => {
+    listaSocios = await obtenerDatos('listar-comercios');
+    mostrarDatos();
+};
 
-btnEliminar.addEventListener('click', () => {
-    Swal.fire({
-        'icon': 'error',
-        'title': 'Socio Comercial Eliminado',
+const obtenerProvincia = (pidProvincia) => {
+    let provinciacomercio;
+    switch (pidProvincia) {
+        case 1:
+            provinciacomercio = 'San José'
+            break;
 
-    });
-});
+        case 2:
+            provinciacomercio = 'Alajuela'
+            break;
+        case 3:
+            provinciacomercio = 'Cartago'
+            break;
+        case 4:
+            provinciacomercio = 'Heredia'
+            break;
+        case 5:
+            provinciacomercio = 'Guanacaste'
+            break;
+        case 6:
+            provinciacomercio = 'Puntarenas'
+            break;
+        case 7:
+            provinciacomercio = 'Limón'
+            break;
+    }
+
+    return provinciacomercio;
+}
+
+// btnEditar.addEventListener('click', () => {
+//     Swal.fire({
+//         'icon': 'info',
+//         'title': 'Botón de editar seleccionado',
+//         'text': 'Redirigiendo a la pagina de edición de socios'
+//     });
+// });
+
+// btnEliminar.addEventListener('click', () => {
+//     Swal.fire({
+//         'icon': 'error',
+//         'title': 'Socio Comercial Eliminado',
+
+//     });
+// });
 
 btnAgregar.addEventListener('click', () => {
     Swal.fire({
@@ -28,6 +65,38 @@ btnAgregar.addEventListener('click', () => {
     });
 });
 
+const mostrarDatos = () => {
+    cuerpoTabla.innerHTML = '';
+    let filtro = document.getElementById('txt-filtro').value.toLowerCase();
+    listaSocios.forEach(socio => {
+        if (socio.nombrecomercio.toLowerCase().includes(filtro)) {
+            let fila = cuerpoTabla.insertRow();
+
+            fila.insertCell().innerText = socio.nombrecomercio;
+            fila.insertCell().innerText = socio.fotocomercio;
+            fila.insertCell().innerText = socio.provinciacomercio;
+            fila.insertCell().innerText = socio.cantoncomercio;
+            fila.insertCell().innerText = socio.distritocomercio;
+
+        }
+    });
+};
+inicializar();
+document.getElementById('txt-filtro').addEventListener('keyup', mostrarDatos);
+
+
+
+
+
+
 //.then(() => {
 //   window.location.href = 'editar_primer_socio.html'
 // });
+
+/*img src="../imgs/usuarioSinFoto.png" alt="img-tercer-socio" class="img">
+<br>
+<p>Punto de retiro: Provincia, Cantón, Distrito</p>
+<br>
+<button type="button" id="btn-editar-socio" class="btn-oscuro">Editar Socio Comercial</button>
+<br><br>
+<button type="button" id="btn-eliminar-socio" class="btn-claro">Eliminar Socio Comercial</button> */
